2c2db69ebd3f37af0f42fdab2560fae637cdf176
Jambi <iturkas42@gmail.com>
2024-05-24 20:49:24 +0300

    Інкапсуляція 2

--- practical_turka2/practical_turka2.cpp
+++ practical_turka2/practical_turka2.cpp
@@ -1,93 +1,90 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
-class Triangle {
+class Student {
 
 private:
 
-    double side1;
-    double side2;
-    double side3;
-
-    bool isValidTriangle(double a, double b, double c) const {
-
-        return (a + b > c) && (a + c > b) && (b + c > a);
-    }
+    string name;
+    int age;
+    double averageGrade;
 
 public:
 
-    Triangle(double a, double b, double c) {
+    Student(const string& n, int a, double g) {
 
-        if (isValidTriangle(a, b, c)) {
+        setName(n);
+        setAge(a);
+        setAverageGrade(g);
+    }
 
-            side1 = a;
-            side2 = b;
-            side3 = c;
+    void setName(const string& n) {
+
+        name = n;
+    }
+
+    void setAge(int a) {
+
+        if (a >= 0) {
+
+            age = a;
         }
 
         else {
 
-            throw invalid_argument("Неправильнi сторони для трикутника.");
+            throw invalid_argument("Вiк не може бути негативним.");
         }
     }
 
-    double getPerimeter() const {
+    void setAverageGrade(double g) {
 
-        return side1 + side2 + side3;
+        if (g >= 0.0 && g <= 100.0) {
+
+            averageGrade = g;
+        }
+
+        else {
+
+            throw invalid_argument("Середнiй бал має бути в дiапазонi вiд 0 до 100.");
+        }
     }
 
-    double getArea() const {
+    string getName() const {
 
-        double s = getPerimeter() / 2;
-
-        return sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        return name;
     }
 
-    bool isRightTriangle() const {
+    int getAge() const {
 
-        double a2 = side1 * side1;
-        double b2 = side2 * side2;
-        double c2 = side3 * side3;
-
-        return (fabs(a2 + b2 - c2) < 1e-10) || (fabs(a2 + c2 - b2) < 1e-10) || (fabs(b2 + c2 - a2) < 1e-10);
+        return age;
     }
 
-    double getSide1() const { return side1; }
-    double getSide2() const { return side2; }
-    double getSide3() const { return side3; }
+    double getAverageGrade() const {
+
+        return averageGrade;
+    }
+
+    void displayInfo() const {
+
+        cout << "Iм'я: " << name << '\n';
+        cout << "Вiк: " << age << '\n';
+        cout << "Середнiй бал: " << averageGrade << '\n';
+    }
 };
 
 int main() {
 
     setlocale(LC_CTYPE, "ukr");
 
-    double a, b, c;
+    Student student("Iван Iванко", 20, 85.5);
 
-    cout << "Введiть довжини сторiн трикутника:" << '\n';
-    cout << "Сторона 1: ";
-    cin >> a;
-    cout << "Сторона 2: ";
-    cin >> b;
-    cout << "Сторона 3: ";
-    cin >> c;
+    student.displayInfo();
+    cout << '\n';
 
-    try {
-
-        Triangle t(a, b, c);
-
-        cout << "Периметр: " << t.getPerimeter() << '\n';
-        cout << "Площа: " << t.getArea() << '\n';
-        cout << "Прямокутний трикутник: " << (t.isRightTriangle() ? "Так" : "Ні") << '\n';
-    }
-    catch (const invalid_argument& e) {
-
-        cerr << e.what() << '\n';
-    }
-     
     return 0;
-}
+} 
 
 
 
